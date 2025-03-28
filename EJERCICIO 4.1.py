f77b7eda37dd2dbb4025b38c2a5a2a3aad8fab01
Liz3026 <ucx_roca_lsca_21@ucalmecac.edu.mx>
2025-03-27 21:55:07 -0600

    Delete EJERCICIO 4.1.py

--- EJERCICIO 4.1.py
+++
@@ -1,6 +0,0 @@
-palabra = input("Escribe una frase: ")
-contador = 0
-for letra in palabra:
-    if letra.isalpha():  # Solo cuenta letras
-        contador += 1
-print("Numero de letras:", contador)
