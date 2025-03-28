1b5c9b59648ad028d47d4f652d0f7d37d054a2b5
Liz3026 <ucx_roca_lsca_21@ucalmecac.edu.mx>
2025-03-27 21:55:22 -0600

    Delete EJERCICIO 4.2.py

--- EJERCICIO 4.2.py
+++
@@ -1,7 +0,0 @@
-palabra = input("Escribe una frase: ")
-contador = 0
-vocales = "aeiouAEIOU"
-for letra in palabra:
-    if letra in vocales:
-        contador += 1
-print("Numero de vocales:", contador)
